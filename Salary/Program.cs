7fdd31ded8b77f1c3238e7c2f6ab8daf5b184c10
RegisSaffi <regissaffi@gmail.com>
2019-11-24 12:20:30 +0200

    First commit

---
+++ Salary/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, id, position;
+            var type=0;
+            double gs = 0;
+            
+            Console.WriteLine("Enter employee id: ");
+            id = Console.ReadLine();
+            Console.WriteLine("Enter employee name: ");
+            name= Console.ReadLine();
+            Console.WriteLine("Enter employee position: ");
+            position = Console.ReadLine();
+            
+            Console.WriteLine("Enter employee gross salary: ");
+            try
+            {
+                gs = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please enter valid gross salary");
+            }
+
+            Console.WriteLine("SELECT EMPLOYEE TYPE:\n");
+
+            Console.WriteLine("1 for monthly employee\n");
+            Console.WriteLine("2 for hourly employee\n");
+            Console.WriteLine("3 for one time employee\n");
+            
+            try
+            {
+                type =Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please enter valid gross salary");
+            }
+            
+            var calculator=new SalaryCalculator(id,name,position);
+            
+            if (type == 1)
+            {
+                var sal = SalaryCalculator.CalculateSalary(gs);
+                Console.WriteLine($"ID: {calculator.Id}\n");
+                Console.WriteLine($"Name: {calculator.Name}\n");
+                Console.WriteLine($"Position: {calculator.Position}\n");
+                
+                Console.WriteLine($"Monthly salary: {sal}\n");
+            }else if (type == 2)
+            {
+                var hours=0;
+                Console.WriteLine("Enter hours this employee worked.");
+                
+                try
+                {
+                    hours=Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Please enter valid hours");
+                }
+                
+                var sal = SalaryCalculator.CalculateSalary(gs,hours);
+                
+                Console.WriteLine($"ID: {calculator.Id}\n");
+                Console.WriteLine($"Name: {calculator.Name}\n");
+                Console.WriteLine($"Position: {calculator.Position}\n");
+                
+                Console.WriteLine($"Hourly salary is: {sal}\n");
+            }else if (type == 3)
+            {
+                var sal = SalaryCalculator.CalculateSalary(Convert.ToInt32(gs));
+                Console.WriteLine($"ID: {calculator.Id}\n");
+                Console.WriteLine($"Name: {calculator.Name}\n");
+                Console.WriteLine($"Position: {calculator.Position}\n");
+                
+                Console.WriteLine($"One time salary is: {sal}\n");
+            }
+            else
+            {
+                Console.WriteLine("Unknown option selected.");
+            }
+            
+            
+        }
+    }
+}
---
+++ Salary/SalaryCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Salary
+{
+    public class SalaryCalculator
+    {
+        private string name, id, position;
+        public SalaryCalculator(string id,string name,string position)
+        {
+            this.id = id;
+            this.name = name;
+            this.position = position;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Position
+        {
+            get => position;
+            set => position = value;
+        }
+
+        public static double CalculateSalary(double gs)
+        {
+           
+            var salary = gs - (gs * 0.3) - (gs * 0.05) - (gs * 0.03);
+            return salary;
+        }
+        
+        public static double CalculateSalary(double rate,int hours)
+        {
+            var salary = (rate*hours)- (rate * 0.05) - (rate * 0.03);
+            return salary;
+        }
+        
+        public static double CalculateSalary(int gs)
+        {
+            var salary = gs - (gs * 0.3);
+            return salary;
+        }
+    }
+}
